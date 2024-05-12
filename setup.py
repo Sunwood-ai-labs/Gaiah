bbfba69c1a33524111e5cfd05fffe79987202287
Maki <sunwood.ai.labs@gmail.com>
2024-05-12 18:46:18 +0900

    🔧 (#3, #5) [refactor] setup.pyのバージョン取得ロジックを改善
    - __version__ を直接参照するのではなく、動的にバージョン情報を取得する新しい関数get_versionを定義。
    - READMEとrequirementsの読み込み処理を改善し、セットアッププロセスの簡略化を図る。
    - パッケージのmetadata設定を清潔に整理し、重複や不要なコメントの削除。

--- setup.py
+++ setup.py
@@ -1,50 +1,38 @@
 from setuptools import setup, find_packages
-# from gaiah.version import __version__
+import re
 
-# READMEファイルの内容を読み込む
+def get_version():
+    with open("gaiah/__init__.py", "r") as f:
+        content = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.MULTILINE)
+        if version_match:
+            return version_match.group(1)
+        else:
+            raise RuntimeError("Unable to find version string.")
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# 依存関係を読み込む
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    # パッケージの名前
     name='gaiah',
-    
-    # パッケージのバージョン
-    version="0.2.0",
-    
-    # パッケージに含めるモジュールを自動的に探す
+    version=get_version(),
     packages=find_packages(),
-    
-    # パッケージの分類情報
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Version Control",
     ],
-    
-    # パッケージに含めるデータファイル
     package_data={
         'gaiah': ['README.md',
                   'requirements.txt'],
     },
-    
-    # PyPIに表示される長い説明文
     long_description=long_description,
-    
-    # 長い説明文のフォーマット
     long_description_content_type="text/markdown",
-    
-    # プロジェクトのURL
     url="https://github.com/your_username/gaiah",
-    
-    # パッケージのインストールに必要な依存関係
     install_requires=requirements,
-
-    # エントリーポイントの設定
     entry_points={
         'console_scripts': [
             'gaiah=gaiah.cli:main',
