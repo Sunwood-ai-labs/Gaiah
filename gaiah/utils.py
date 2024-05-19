716464f1e556f70c9fa09122d601b823f1bbea0f
Maki <sunwood.ai.labs@gmail.com>
2024-05-19 10:11:30 +0900

    🔨 [chore] コマンド実行ユーティリティ関数の追加
    - Git コマンドなどの外部コマンドを実行するためのヘルパー関数 `run_command` を導入。
    - 実行中のコマンドとその結果をログ出力する機能を備え、エラー発生時には詳細なエラーメッセージを提供。
    - システムコールの例外処理を強化し、安全なコマンド実行を保証。

---
+++ gaiah/utils.py
@@ -0,0 +1,16 @@
+import subprocess
+from loguru import logger
+
+def run_command(command, cwd=None, check=True):
+    """
+    コマンドを実行し、結果を返す
+    """
+    try:
+        logger.info(f"実行コマンド： {' '.join(command)}")
+        result = subprocess.run(command, cwd=cwd, check=check, capture_output=True, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        error_message = e.stderr.strip()
+        logger.error(f"Error while running command: {' '.join(command)}")
+        logger.error(f"Error message: {error_message}")
+        raise
