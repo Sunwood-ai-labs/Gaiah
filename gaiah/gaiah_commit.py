9a28f6fa182e9cdf160f3ee82c2f830c005a799a
Maki <sunwood.ai.labs@gmail.com>
2024-05-31 23:45:00 +0900

    🛠️ [refactor] ファイルの状態変更時のロギングを改善
    - ファイルが変更されたときと削除されたときに、それぞれの状態をログに記録するようにしました。これにより、デバッグと監視が容易になります。
    - 不要な `unstage_files()` の呼び出しをコメントアウトし、誤ってステージング解除を防ぎます。これにより、安定したコミットプロセスが保証されます。

--- gaiah/gaiah_commit.py
+++ gaiah/gaiah_commit.py
@@ -142,8 +142,10 @@ class GaiahCommit:
         try:
             if os.path.exists(os.path.join(self.repo.repo_dir, filename)):
                 self.stage_file(filename, "modified")
+                self.logger.info("file is modified")
             else:
                 self.stage_file(filename, "deleted")
+                self.logger.info("file is deleted")
 
             changed_files = run_command(["git", "diff", "--staged", "--name-only"], cwd=self.repo.repo_dir).splitlines()
 
@@ -152,7 +154,7 @@ class GaiahCommit:
                 # self.repo.push_to_remote(branch_name=branch_name)
             else:
                 self.logger.info(f"No changes detected in file: {filename}")
-                self.unstage_files()
+                # self.unstage_files()
 
         except Exception as e:
             self.logger.error(f"Error while processing file: {filename} - {e}")
