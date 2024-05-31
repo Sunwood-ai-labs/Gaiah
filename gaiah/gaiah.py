8cee21e941ddc090b949bd2f952b65dd32a498f7
Maki <sunwood.ai.labs@gmail.com>
2024-06-01 00:14:01 +0900

    🔧 [refactor] Gaiahクラスの初期化と実行プロセスの再構築
    - コンストラクタに引数オブジェクトを渡すことで、初期化時の処理を集約しました。
    - 新たに `run` メソッドを実装し、リポジトリの初期化、リモートリポジトリの作成、コミット処理を一元管理します。
    - ロジックの整理により、コードのモジュール性と拡張性が向上しています。

--- gaiah/gaiah.py
+++ gaiah/gaiah.py
@@ -3,11 +3,28 @@ from .gaiah_repo import GaiahRepo
 from .gaiah_commit import GaiahCommit
 
 class Gaiah:
-    def __init__(self, repo_dir, commit_messages_path):
-        self.repo_dir = repo_dir
-        self.commit_messages_path = commit_messages_path
-        self.repo = GaiahRepo(repo_dir, commit_messages_path)
+    def __init__(self, args):
+        self.args = args
+        self.repo_dir = args.repo_dir if args.init_repo or args.process_commits else None
+        self.commit_msg_path = args.commit_msg_path if args.process_commits else None
+        self.repo = GaiahRepo(self.repo_dir, self.commit_msg_path)
         self.commit = GaiahCommit(self.repo)
+
+    def run(self):
+        if self.args.init_repo:
+            self.init_local_repo(self.args.repo_dir, not self.args.no_initial_commit)
+
+        if self.args.create_repo:
+            repo_params = {
+                'description': self.args.description,
+                'private': self.args.private
+            }
+            self.create_remote_repo(self.args.repo_name, repo_params)
+
+        if self.args.process_commits:
+            self.process_commits()
+
+        logger.success("successfully!")
 
     def init_local_repo(self, repo_dir, initial_commit=True):
         self.repo.init_local_repo(repo_dir, initial_commit)
