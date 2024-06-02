4edec3fc0e5f0f5d64b9e2edacd53ea045175352
Maki <sunwood.ai.labs@gmail.com>
2024-06-02 19:10:15 +0900

    🔧 [refactor] Gaiah CLI を設定ファイル駆動に変更
    - コマンドライン引数を廃止し、設定ファイル (.gaiah/config.yml) から設定を読み込むように変更しました。
    - 設定ファイルが存在しない場合は、テンプレートファイルをコピーします。

--- gaiah/cli.py
+++ gaiah/cli.py
@@ -3,6 +3,9 @@ from art import tprint
 from loguru import logger
 from gaiah.gaiah import Gaiah
 import sys
+import os
+import yaml
+import shutil
 
 logger.configure(
     handlers=[
@@ -20,26 +23,51 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(description='Gaiah - シンプルなGitリポジトリ管理ツール')
     
-    parser.add_argument('--create_repo', action='store_true', help='新しいリポジトリを作成する')
-    parser.add_argument('--repo_name', help='作成するリポジトリ名')
-    parser.add_argument('--description', default='', help='リポジトリの説明')
-    parser.add_argument('--private', action='store_true', help='プライベートリポジトリとして作成する')
-    
-    parser.add_argument('--init_repo', action='store_true', help='既存のディレクトリをGitリポジトリとして初期化する')
-    parser.add_argument('--repo_dir', default='./', help='初期化するディレクトリのパス')
-    parser.add_argument('--no_initial_commit', action='store_true', help='初期コミットを作成しない')
-    
-    parser.add_argument('--branch_name', default=None, help='コミットに使用するブランチ名')
-    parser.add_argument('--process_commits', action='store_true', help='マークダウンファイルから複数のコミットを行う')
-    parser.add_argument('--commit_msg_path', default='.Gaiah.md', help='コミットメッセージファイルのパス')
-
+    parser.add_argument('--config', default='.gaiah/config.yml', help='設定ファイルのパス')
+    parser.add_argument('--mode', default='commit', help='処理モード')
     return parser.parse_args()
+
+def load_config(config_path):
+    """
+    設定ファイルを読み込む
+    """
+    if not os.path.exists(config_path):
+        # 設定ファイルが存在しない場合はテンプレートをコピー
+        template_path = os.path.join(os.path.dirname(__file__), 'template', 'config.example.yml')
+        os.makedirs(os.path.dirname(config_path), exist_ok=True)
+        shutil.copyfile(template_path, config_path)
+        logger.info(f"設定ファイルが存在しないため、テンプレートをコピーしました: {config_path}")
+
+    with open(config_path, 'r', encoding="utf8") as f:
+        config = yaml.safe_load(f)
+        
+    return config
+
+def merge_configs(base_config, override_config):
+    merged_config = base_config.copy()
+    for key, value in override_config.items():
+        if isinstance(value, dict):
+            merged_config[key] = merge_configs(merged_config.get(key, {}), value)
+        else:
+            merged_config[key] = value
+    return merged_config
 
 def main():
     args = parse_arguments()
-    tprint("!  Welcome to Gaiah  !")
-    gaiah = Gaiah(args)
-    gaiah.run()
+    gaiah_config = load_config(args.config)
+    tprint("!   Welcome  to  Gaiah   !")
+        
+    if(args.mode == "make"):
+        logger.info("mode is << make >>")
+        gaiah_config["gaiah"] = merge_configs(gaiah_config["gaiah"], gaiah_config["gaiah"]["init"])
+        gaiah = Gaiah(gaiah_config)
+        gaiah.run()
+        
+    if(args.mode == "commit"):
+        logger.info("mode is << commit >>")
+        gaiah_config["gaiah"] = merge_configs(gaiah_config["gaiah"], gaiah_config["gaiah"]["dev"])
+        gaiah = Gaiah(gaiah_config)
+        gaiah.run()
 
 if __name__ == "__main__":
     main()
